ce1f976e77634aee38487e33c296a1c2a8b6e15d
teo3300 <matteo.rogora@live.it>
2020-06-22 14:27:15 +0200

    Update extractor.py

--- extractor.py
+++ extractor.py
@@ -4,6 +4,7 @@ def slot(mem, pos, size):
     for i in range(pos+size-1, pos-1, -1):
         value = (value<<8) + mem[i]
     return value
+    # Read <size> elements (bytes) from list as single value
 
 def loadData(mem):
     if not slot(mem, 0, 2) == 19778:
@@ -13,9 +14,9 @@ def loadData(mem):
     if (not bpp == 16):
         print('ERROR: bitdepth must be 16bpp, found', bpp +'bpp')
         exit()
-
     img_offset = slot(mem,10,4)
     img_width  = slot(mem,18,4)
     img_height = slot(mem,22,4)
     img_size   = slot(mem,34,4)
     return (img_offset, img_size, img_width, img_height)
+    # Return image info
