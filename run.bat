d3eff939637e99db1899a4bff27dea64b76b89de
teo3300 <matteo.rogora@live.it>
2020-11-14 16:18:41 +0100

    what am I doing with my life

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-python bmp2c.py ./img/4bpp/font_test.bmp ./source 4 8
-python bmp2c.py ./img/4bpp/font_test.bmp ./source 8 8
+python bmp2c.py ./img/font_test.bmp ./source 4 8
+python bmp2c.py ./img/font_test.bmp ./source 8 8
 :: python, name, source_file, dest_folder, bitdepth split_size (0 not split) default 8
 PAUSE
